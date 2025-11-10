a100dcf610d6819db04027bd22164eb0b0b90b02
JoshucanTech <joshucantech@gmail.com>
2025-11-10 18:27:12 +0100

    finally running the cron job

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { DatabaseHealthService } from './database-health.service';
+import { Public } from 'src/auth/decorators/public.decorator';
+import { Interval } from '@nestjs/schedule';
 
 @ApiTags('Health')
 @Controller('health')
@@ -11,14 +13,16 @@ export class HealthController {
   async checkDatabaseHealth() {
     return await this.databaseHealthService.checkDatabaseHealth();
   }
-  
+
   @Get('database/latest')
   getLatestDatabaseHealth() {
     return this.databaseHealthService.getLatestHealthCheck();
   }
-  
+
   @Get('database/cron')
+  @Interval(240000) // 240 seconds
+  @Public()
   async triggerDatabaseHealthCheck() {
-    return await this.databaseHealthService.checkDatabaseHealth();
+    return await this.databaseHealthService.checkDatabaseHealthCron();
   }
 }
